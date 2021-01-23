7db46dce0ae6c1705914dc78d2ab6ff74a7d1784
Santosh2019 <santoshlimbale76@gmail.com>
2021-01-23 19:51:47 +0530

    Second Commit

---
+++ src/main/java/com/example/Test/EmployeeController/EmpController.java
@@ -0,0 +1,56 @@
+package com.example.Test.EmployeeController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Test.SrvcImpl.BookEmployeeServiceImpl;
+import com.example.Test.bean.Employee;
+
+@RestController
+public class EmpController {
+
+	@Autowired
+	BookEmployeeServiceImpl serviceImpl;
+
+	@GetMapping("/singleEmployee/{employeeId}")
+	public Employee getEmployee(@PathVariable("employeeId") long employeeId) {
+		return serviceImpl.getSingleEmployee(employeeId);
+	}
+
+	@GetMapping("/employeeList")
+	public List<Employee> getEmployee(Employee employee) {
+
+		return serviceImpl.getAllEmployee(employee);
+	}
+
+	@PostMapping("/create")
+	public void createEmployee(@RequestBody Employee employee) {
+
+		serviceImpl.addEmployee(employee);
+
+	}
+
+	
+	/*Update Employee data*/
+	@PutMapping("/update/{employeeId}")
+	public void updateEmployee(@PathVariable("employeeId") long employeeId, @RequestBody Employee employee) {
+
+		serviceImpl.updateEmployee(employee);
+	}
+
+	
+
+	@DeleteMapping("/remove/{employeeId}")
+	public void deleteEmployee(@PathVariable("employeeId") long employeeId) {
+		serviceImpl.deleteEmployee(employeeId);
+	}
+
+}
---
+++ src/main/java/com/example/Test/bean/Employee.java
@@ -0,0 +1,74 @@
+package com.example.Test.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee_tble")
+public class Employee {
+
+	@Id
+	private long employeeId;
+
+	private String employeeName;
+
+	private String employeeLocation;
+
+	private String employeeDesignation;
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(long employeeId, String employeeName, String employeeLocation, String employeeDesignation) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeLocation = employeeLocation;
+		this.employeeDesignation = employeeDesignation;
+	}
+
+	public long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public String getEmployeeLocation() {
+		return employeeLocation;
+	}
+
+	public void setEmployeeLocation(String employeeLocation) {
+		this.employeeLocation = employeeLocation;
+	}
+
+	public String getEmployeeDesignation() {
+		return employeeDesignation;
+	}
+
+	public void setEmployeeDesignation(String employeeDesignation) {
+		this.employeeDesignation = employeeDesignation;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeLocation="
+				+ employeeLocation + ", employeeDesignation=" + employeeDesignation + "]";
+	}
+
+
+	
+	
+}
